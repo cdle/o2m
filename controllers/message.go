d2f71de301fd183b9d90572fdc985c76debba771
邰胜广 <cdle@taishengguangdeMacBook-Pro.local>
2021-01-28 11:05:23 +0800

    Initial commit

--- controllers/message.go
+++ controllers/message.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/cdle/o2m/models"
@@ -63,6 +64,7 @@ func (c *MessageController) GetClientHistoryMessage() {
 // @router /polling [get]
 func (c *MessageController) ReceiveMessage() {
 	c.getAuth()
+
 	ap := &models.AjaxPolling{}
 	u := c.User()
 	u.Note()
@@ -97,6 +99,7 @@ func (c *MessageController) getAuth() {
 		if c.GetString("role") != "" {
 			c.Allow(models.UserRoleServer)
 		}
+		c.Ctx.Output.Header("uid", fmt.Sprint(c.UID))
 		return
 	} else if c.GetString("role") != "" {
 		c.Allow(models.UserRoleServer)
@@ -111,5 +114,6 @@ func (c *MessageController) getAuth() {
 	c.ResponseError(models.CreateUser(u, nil))
 	c.SetSession("uid", u.ID)
 	c.SetSession("utp", u.Role)
+	c.Ctx.Output.Header("uid", fmt.Sprint(u.ID))
 	c.Response()
 }
